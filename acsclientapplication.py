e3ad02ce8737d8b7d8bbe850064db1eb82c139fc
ki1red <druzhinin_nikita@list.ru>
2024-06-06 20:42:15 +0700

    добавлен задний фон

--- acsclientapplication.py
+++ acsclientapplication.py
@@ -2,8 +2,10 @@ import subprocess
 import time
 import tkinter as tk
 import threading
+from PIL import Image, ImageTk
 
-rtmp_url = "rtmp://localhost:1935/live/stream"
+rtmp_url = "rtmp://158.160.167.74:1935/live/stream"
+image_path = "default.jpg"
 
 # Функция для закрытия окна Tkinter
 def close_window():
@@ -11,10 +13,19 @@ def close_window():
 
 # Функция для отображения окна Tkinter с изображением
 def display_image():
-    global root
+    global root, label
     root = tk.Tk()
     root.attributes("-fullscreen", True)  # Отображаем окно на весь экран
     root.configure(bg='black')  # Устанавливаем черный фон окна
+
+    img = Image.open(image_path)
+    img = img.resize((root.winfo_screenwidth(), root.winfo_screenheight()), Image.LANCZOS)
+    photo = ImageTk.PhotoImage(img)
+
+    label = tk.Label(root, image=photo)
+    label.image = photo
+    label.pack()
+
     # Запускаем цикл Tkinter
     root.mainloop()
 
